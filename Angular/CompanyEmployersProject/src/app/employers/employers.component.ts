7b380e094fe485935f09fb57412c3c5d9e64cbc5
matanSH1996 <87108739+matanSH1996@users.noreply.github.com>
2021-12-25 10:48:06 +0200

    25.12 - 1100

--- Angular/CompanyEmployersProject/src/app/employers/employers.component.ts
+++ Angular/CompanyEmployersProject/src/app/employers/employers.component.ts
@@ -23,40 +23,4 @@ export class EmployersComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // employee : Employee = {
-  //   id : 205962178,
-  //   firstName : 'matan',
-  //   lastName : 'shtern',
-  //   dateOfBirth : '02.02.1996',
-  //   salary : 8000,
-  //   email : 'matan.shtern2@gmail.com'
-  // };
-
-  // employee1 : Employee = {
-  //   id : 312030405,
-  //   firstName : 'dror',
-  //   lastName : 'abas',
-  //   dateOfBirth : '01.01.1997',
-  //   salary : 8000,
-  //   email : 'dror.abas@gmail.com'
-  // };
-
-  // employee2 : Employee = {
-  //   id : 252030405,
-  //   firstName : 'liran',
-  //   lastName : 'levi',
-  //   dateOfBirth : '26.08.1997',
-  //   salary : 10000,
-  //   email : 'liraniba@gmail.com'
-  // };
-
-  // employee3 : Employee = {
-  //   id : 12345678,
-  //   firstName : 'lavi',
-  //   lastName : 'shtern',
-  //   dateOfBirth : '15.04.2000',
-  //   salary : 12000,
-  //   email : 'laviSH@gmail.com'
-  // };
-
 }
